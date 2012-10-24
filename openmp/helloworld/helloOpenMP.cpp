6fad78d1ffcd60a34ee4a238599495dc5294c4a5
olivererxleben <oliver.erxleben@me.com>
2012-10-24 17:25:45 +0200

    added openmp hello world example

---
+++ openmp/helloworld/helloOpenMP.cpp
@@ -0,0 +1,20 @@
+/* 
+	* 
+	* Hello World programm prints 'Hello World' message x-times, where x is number of threads in CPU
+	* @version 1.0
+	*
+	* compiled with: 	g++ -fopenmp helloOpenMP.cpp -o test
+	* running with:		./test
+ */
+
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+	// Will print num Threads available on cpu
+	#pragma omp parallel
+	{
+		printf("Hello World\n");
+	}
+}
