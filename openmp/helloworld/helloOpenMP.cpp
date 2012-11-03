ca15c728f845960151faff824673b50d850ef1a0
olivererxleben <oliver.erxleben@me.com>
2012-11-03 19:06:21 +0100

    OpenMP Eigenschafteno zum holen von Prozessorinfos und Setzen von Prozessoranzahl hinzugefügt

--- openmp/helloworld/helloOpenMP.cpp
+++ openmp/helloworld/helloOpenMP.cpp
@@ -12,11 +12,24 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define NUM_PROCESSORS 5
+
 int main(int argc, char *argv[])
 {
+	
+	int tid; // thread id
+	
+	printf( "Number of Processors/threads in this System: %i\nNumber of Threads use in this Program: %i\n\n", omp_get_num_procs(), NUM_PROCESSORS );
+	
+	// the next parallel block will be executes with the given value 
+	omp_set_num_threads(NUM_PROCESSORS);
+	
 	// Will print num Threads available on cpu
-	#pragma omp parallel
+	// 'omp parallel private' will give you access to private variables like thread_id(tid)
+	// each thread has its own private tid 
+	#pragma omp parallel private(tid)
 	{
-		printf("Hello World\n");
+		tid = omp_get_thread_num();
+		printf("Hello World, created by thread nr %i\n", tid);
 	}
 }
