81c1bd2fb6e069e4e1fea8b09596d066a57279f6
olivererxleben <oliver.erxleben@me.com>
2012-12-02 20:51:25 +0100

    Code für Listing angepasst

--- hausarbeit/src/1_3_example/parallel.c
+++ hausarbeit/src/1_3_example/parallel.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define NUM_THREADS 2
-
-int main(int argc, char *argv[]) 
-{
-	
-	omp_set_num_threads(NUM_THREADS);
-	
-	int i = 0;
+int main(int argc, char *argv[]) {
 	#pragma omp parallel 
 	{
-		for (i; i < 10; i++)
-		{
-			printf("%i \n", i);
-		}
+		int i;
+		for (i = 0; i < 10; i++) { /* some block code */ }
 	}
-	
 	return 0;
 }
