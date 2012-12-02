923654e98051a41e2cfc98eaa6cd18df62d37630
olivererxleben <oliver.erxleben@me.com>
2012-12-02 20:54:35 +0100

    parallelfor.c added
    Parallel for - code: Unterschied zwischen parallel und parallel for
    aufzeigen

---
+++ hausarbeit/src/1_3_example/parallelfor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+	
+	#pragma omp parallel for
+	{
+		int i;
+		for (i = 0; i < 10; i++) { /* some block code */ }
+	}
+	return 0;
+}
