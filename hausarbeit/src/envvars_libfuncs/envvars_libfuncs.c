0067f6ea8024e8c5f62d3658dcc705be6254325f
olivererxleben <oliver.erxleben@me.com>
2013-01-24 16:56:27 +0100

    src structure

---
+++ hausarbeit/src/envvars_libfuncs/envvars_libfuncs.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main (void) {
+	
+	return 0;
+}
---
+++ hausarbeit/src/hello_world/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#ifdef _OPENMP
+ # include <omp.h>
+#endif
+
+int main (void) {
+	
+	
+	
+	return 0;
+}
